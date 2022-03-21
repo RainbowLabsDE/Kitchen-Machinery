532fb413339e0b9aeffeffe1a9d4fb139a559f2f
LeoDJ <leandro.spaeth@gmail.com>
2022-03-21 05:39:44 +0100

    display init works

--- Display/Software/src/main.cpp
+++ Display/Software/src/main.cpp
@@ -1,9 +1,27 @@
-#include <Arduino.h>
+#include <TFT_eSPI.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Pin definitions and other options are inside platformio.ini
+TFT_eSPI tft = TFT_eSPI();
+
+void setup(void) {
+    tft.init();
+    tft.initDMA();
+    tft.setRotation(3);
+    tft.fillScreen(TFT_BLACK);
+
+    // Set "cursor" at top left corner of display (0,0) and select font 4
+    tft.setCursor(0, 0, 4);
+
+    tft.setTextColor(TFT_RED, TFT_BLACK);
+    tft.print("Hello ");
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.print("World");
+    tft.setTextColor(TFT_BLUE, TFT_BLACK);
+    tft.println("!");
+    delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    tft.print(".");
+    delay(500);
 }
